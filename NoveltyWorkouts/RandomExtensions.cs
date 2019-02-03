ae20e16238d7d490d3e242d98d75206fa23d8502
Eric Winkler <eric@winkler.net.au>
2019-02-03 13:06:43 +0800

    Following the naming rules

--- NoveltyWorkouts/RandomExtensions.cs
+++ NoveltyWorkouts/RandomExtensions.cs
@@ -6,7 +6,7 @@ namespace NoveltyWorkouts
 {
     internal static class RandomExtensions
     {
-        private static Random random = new Random();
-        public static T PickRandom<T>(this IEnumerable<T> choices) => choices.OrderBy(o => random.Next()).First();
+        private static Random _random = new Random();
+        public static T PickRandom<T>(this IEnumerable<T> choices) => choices.OrderBy(o => _random.Next()).First();
     }
 }
